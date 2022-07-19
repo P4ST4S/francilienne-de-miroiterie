7116229cbd66ee73851b5c039c577d8030930acf
P4ST4S <arospars77@gmail.com>
2022-07-19 12:48:03 +0200

    legal mentions

--- src/pages/LegalMentions.js
+++ src/pages/LegalMentions.js
@@ -1,7 +1,220 @@
 import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+import classes from "../styles/pages/LegalMentions.module.scss";
 
 const LegalMentions = () => {
-  return <div>LegalMentions</div>;
+  return (
+    <div className={classes.lm}>
+      <div className={classes.lm__content}>
+        <h1>Mentions légales</h1>
+        <summary>
+          <h2>Sommaires :</h2>
+          <ol>
+            <li>
+              <AnchorLink href="#1">Informations légales</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#2">
+                Directeur de la publication et de la rédaction
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#3">Développement/Graphisme</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#4">Accès au contenu</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#5">Crédits photos</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#6">Propriété intellectuelle</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#7">
+                Liens hypertextes vers d&#39;autres sites
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#8">
+                Responsabilité de Francilienne de Miroiterie
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#9">Droit applicable</AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#10">
+                Modification des mentions légales
+              </AnchorLink>
+            </li>
+          </ol>
+        </summary>
+        <h3 id="1">Informations légales</h3>
+        <p>
+          Le site www.francilienne-de-miroiterie.com est édité par : La société
+          Francilienne de Miroiterie
+        </p>
+        <br />
+        <ul>
+          <li>Forme juridique : Société A Responsabilité Limitée</li>
+          <li>Siège social : 16, rue de Berlin - 77144 Montévrain.</li>
+          <li>Téléphone : 01.60.36.19.59</li>
+          <li>N° RCS : 347 731 267</li>
+          <li>Code APE : 4334Z</li>
+          <li>Capital social : 15 244.90€</li>
+        </ul>
+        <br />
+        <h3 id="2">Directeur de la publication et de la rédaction</h3>
+        <p>David Rospars</p>
+        <br />
+        <h3 id="3">Développement/Graphisme</h3>
+        <p>Antoine Rospars</p>
+        <br />
+        <h3>Hébergement</h3>
+        <p>
+          Le prestataire assurant l’hébergement des sites et le stockage direct
+          et permanent des informations est :
+        </p>
+        <br />
+        <ul>
+          <li>Société OVH</li>
+          <li>Adresse : 2 rue Kellermann - 59100 Roubaix - France</li>
+          <li>Téléphone service client: 1007</li>
+        </ul>
+        <br />
+        <h3 id="4">Accès au contenu</h3>
+        <p>
+          L’utilisateur reconnaît avoir pris connaissance de la présente notice
+          légale et s’engage à en respecter les dispositions.
+        </p>
+        <p>
+          L’accès au site web est libre. L’utilisateur du site reconnaît
+          disposer de la compétence et des moyens nécessaires pour accéder et
+          utiliser ce site. L’utilisateur reconnaît avoir vérifié que la
+          configuration informatique utilisée ne contient aucun virus et qu’elle
+          est en parfait état de fonctionnement.
+        </p>
+        <p>
+          L’utilisateur s’engage à n’utiliser les services ainsi que l’ensemble
+          des informations auxquelles il pourra avoir accès que pour des raisons
+          personnelles et dans un but conforme à l’ordre public, aux bonnes
+          mœurs et aux droits des tiers.
+        </p>
+        <p>
+          L’utilisateur s’engage à ne commettre aucun acte pouvant mettre en
+          cause la sécurité informatique des sites web ou du système
+          informatique des autres utilisateurs.
+        </p>
+        <p>
+          L’utilisateur s’engage à ne pas interférer ou interrompre le
+          fonctionnement normal des sites. L’utilisateur s’engage lorsque cela
+          lui est demandé à ne renseigner que des informations le concernant,
+          complètes, exactes et à jour.
+        </p>
+        <br />
+        <h3 id="5">Crédits photos</h3>
+        <p>
+          Toutes les images présentes sur ce site web sont soumises à un
+          copyright.
+        </p>
+        <p>
+          L&#39;intégralité des images et photos de ce site web ne sont pas
+          contractuelles.
+        </p>
+        <br />
+        <h3 id="6">Propriété intellectuelle</h3>
+        <p>
+          Le Site est la propriété exclusive de la société Francilienne de
+          Miroiterie, seule habilitée à utiliser et exploiter les droits de
+          propriété intellectuelle et droits de la personnalité attachés à ce
+          site, notamment les marques, dessins et modèles, droits d&#39;auteur
+          et droit à l&#39;image, à titre originaire ou par l&#39;effet
+          d&#39;une cession ou d&#39;une licence d&#39;exploitation.
+        </p>
+        <br />
+        <p>
+          L&#39;usage de tout ou partie du Site
+          www.francilienne-de-miroiterie.com, notamment par téléchargement,
+          reproduction, transmission ou représentation à d&#39;autres fins que
+          pour usage personnel et privé dans un but non commercial est
+          strictement interdit.
+        </p>
+        <br />
+        <p>
+          Les bases de données figurant sur le Site sont protégées par le Code
+          de la propriété intellectuelle et est sanctionnée toute extraction ou
+          réutilisation qualitativement ou quantitativement substantielle du
+          contenu des bases de données.
+        </p>
+        <br />
+        <h3 id="7">Liens hypertextes vers d&#39;autres sites</h3>
+        <p>
+          La création de liens hypertextes vers le Site ne peut être faite
+          qu&#39;avec l&#39;autorisation écrite et préalable de la société
+          Francilienne de Miroiterie qui pourra être révoquée à tout moment. La
+          société Francilienne de Miroiterie, décline toute responsabilité
+          concernant le contenu des sites liés au Site.
+        </p>
+        <br />
+        <h3 id="8">Responsabilité de Francilienne de Miroiterie</h3>
+        <p>
+          La société Francilienne de Miroiterie met tout en œuvre pour offrir
+          aux utilisateurs des informations ou des outils disponibles et
+          vérifiés mais ne sauraient être tenus pour responsables des erreurs,
+          d’une absence de disponibilité des fonctionnalités ou de la présence
+          de virus ou autres infections logiques sur leurs sites.
+        </p>
+        <br />
+        <p>
+          Le Site est accessible 24 heures sur 24 et 7 jours sur 7. L’éditeur ne
+          saurait toutefois être tenu pour responsables des discontinuités du
+          site et de leurs services, qu’elles soient volontaires ou non. Les
+          informations fournies le sont à titre indicatif et ne sauraient
+          dispenser l’utilisateur d’une analyse complémentaire et personnalisée.
+        </p>
+        <br />
+        <p>
+          Le visiteur est informé que l&#39;accès au Site pourra être interrompu
+          à tout moment par la société Francilienne de Miroiterie pour des
+          raisons de maintenance, de sécurité ou toute autre contrainte
+          technique.
+        </p>
+        <br />
+        <h3 id="9">Droit applicable</h3>
+        <p>
+          Les présentes conditions sont régies par le droit français. Les
+          juridictions françaises sont seules compétentes pour connaître de tout
+          litige se rapportant directement ou indirectement à l&#39;accès au
+          présent Site ou à son utilisation.
+        </p>
+        <p>
+          La version originale de ces conditions est rédigée en français. En cas
+          de conflit ou d&#39;incohérence entre la version française et
+          celle-ci, la version française prévaudra.
+        </p>
+        <br /> <h3 id="10">Modification des mentions légales</h3>
+        <p>
+          L’éditeur se réserve le droit de modifier et d’actualiser les
+          présentes mentions légales à tout moment et sans préavis.
+        </p>
+        <p>L’utilisateur est donc invité à la consulter régulièrement.</p>
+        <p>Date de la dernière mise à jour : 11 juillet 2022</p>
+        <p>
+          Ce site est protégé par reCAPTCHA et les{" "}
+          <a
+            href="https://policies.google.com/privacy?hl=fr"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Règles de Confidentialité et Conditions d&#39;Utilisation
+          </a>{" "}
+          s&#39;appliquent.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LegalMentions;
